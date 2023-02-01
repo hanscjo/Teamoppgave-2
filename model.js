693ae2dd1f763fbc76481c0baf3f90c58920f1f2
hanscjo <johnsen.hans.christian@gmail.com>
2023-02-01 12:55:06 +0100

    img instansiering
    Co-Authored-By: kristian7777 <122276545+kristian7777@users.noreply.github.com>

--- model.js
+++ model.js
@@ -4,16 +4,20 @@ let sleep = 100;
 let love = 100;
 
 let experience = 0;
+let nextLevel = 1000;
+let evolveState = 0;
 
-const moodGifs = ['https://cdn.pixabay.com/photo/2020/12/27/20/24/smile-5865208_1280.png',
-'https://images.emojiterra.com/google/android-11/512px/263a.png',
-'https://www.pngitem.com/pimgs/m/50-500914_neutral-emoji-png-transparent-background-neutral-emoji-png.png',
-'http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/frowning-face.png',
-'https://media.tenor.com/gMC-purKMQ4AAAAM/sad-cry.gif'];
 
-const fishGifs = ['https://cdn.7tv.app/emote/62523dbbbab59cfd1b8b889d/4x.webp', 
-'https://cdn.7tv.app/emote/61f76eb4356ca68083739987/4x.webp', 
-'https://cdn.7tv.app/emote/62994ca54b77f2f753fb827e/4x.webp',
-'https://cdn.7tv.app/emote/62e5e3c105e2ed080d60820e/4x.webp',
-'https://cdn.7tv.app/emote/62982b6dfad983b04450b7d4/4x.webp',
-'https://cdn.7tv.app/emote/631c4a744f3e0f1fc59f83d5/4x.webp'];
+
+let moodState = 0;
+
+const moodGifs = ['img/mood/fiskglad1.1(bilde1)mbakgrunn.png',
+'img/mood/fiskbekymret1(bilde3)mbakgrunn.png',
+'img/mood/fisk kjempe trist1(bilde5)mbakgrunn.png',
+'img/mood/fisksint1(bilde6)mbakgrunn.png',
+'img/mood/fiskdod1(bilde7)alternative.png'];
+
+const fishGifs = ['img/evolution/fiskevolve1(bilde1)mbakgrunn.png',
+'img/evolution/fiskevolve2(bilde2)mbakgrunn.png',
+'img/evolution/fiskevolve3(bilde3)mbakgrunn.png',
+'img/evolution/fiskevolve4(bilde4)mbakgrunn.png'];
